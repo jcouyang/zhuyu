13a841ca75f7052d01727da0a5ae5abf79884630
Scala Steward <me@scala-steward.org>
2021-05-17 23:23:30 +0200

    Update doobie-core, doobie-postgres to 0.13.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   )
 
   lazy val doobie = Seq(
-    "org.tpolecat" %% "doobie-core" % "0.13.2",
+    "org.tpolecat" %% "doobie-core" % "0.13.3",
   )
   lazy val shapeless = Seq(
     "com.chuusai" %% "shapeless" % "2.3.7"
