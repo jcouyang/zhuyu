3d4633c962bfceb5d1754a4bbbd2dec82b783ec6
Scala Steward <me@scala-steward.org>
2021-09-03 00:33:20 +0200

    Update http4s-client, http4s-dsl to 0.23.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   )
 
   lazy val http4s = {
-    val http4sVersion = "0.23.2"
+    val http4sVersion = "0.23.3"
     Seq(
     "org.http4s" %% "http4s-dsl" % http4sVersion,
     "org.http4s" %% "http4s-client" % http4sVersion
