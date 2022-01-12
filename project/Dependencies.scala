231e1f4765c1623f089f436ac80558c4489cbf6c
Scala Steward <me@scala-steward.org>
2022-01-12 22:46:33 +0100

    Update aws-java-sdk-s3, aws-java-sdk-sqs to 1.12.139

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,11 +25,11 @@ object Dependencies {
   }
 
   lazy val awsSqs = Seq(
-    "com.amazonaws" % "aws-java-sdk-sqs" % "1.12.138",
+    "com.amazonaws" % "aws-java-sdk-sqs" % "1.12.139",
     "com.amazonaws" % "amazon-sqs-java-temporary-queues-client" % "1.2.4"
   )
   lazy val awsS3 = Seq(
-    "com.amazonaws" % "aws-java-sdk-s3" % "1.12.138"
+    "com.amazonaws" % "aws-java-sdk-s3" % "1.12.139"
   )
   lazy val ciris = Seq(
     "is.cir"          %% "ciris-cats"          % "0.12.1",
