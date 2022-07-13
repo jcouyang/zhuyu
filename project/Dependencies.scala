b9f4345e23e30290eefacc0137d5282f3426bd77
Scala Steward <scala_steward@virtuslab.com>
2022-07-13 20:29:41 +0000

    Update cats-effect to 3.3.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val cats = Seq(
     "org.typelevel"   %% "cats-core" % "2.7.0",
-    "org.typelevel" %% "cats-effect" % "3.3.11",
+    "org.typelevel" %% "cats-effect" % "3.3.14",
   )
 
   lazy val doobie = Seq(
