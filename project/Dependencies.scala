ecc51ca12e25899ad663c0ae6768da474a4249f3
Scala Steward <scala_steward@virtuslab.com>
2022-08-30 14:28:47 +0000

    Update logback-classic to 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -57,7 +57,7 @@ object Dependencies {
   )
 
   lazy val logback = Seq(
-    "ch.qos.logback"  %  "logback-classic"     % "1.2.11",
+    "ch.qos.logback"  %  "logback-classic"     % "1.4.0",
   )
 
   lazy val scalacheckShapeless = Seq(
